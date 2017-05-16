6c3a1938bde0f5bf7fc981256007ad4cf92abe19
Your Name <arhanace@gmail.com>
2017-05-15 20:54:26 -0500

    Change

--- Main.java
+++ Main.java
@@ -5,12 +5,25 @@ public class Main {
 		// TODO Auto-generated method stub
 		BoardPrinter g = new BoardPrinter();
 		Game s = new Game();
+		WinLose w = new WinLose();
 		g.CreateBoard();
 		g.PrintBoard();
-		
+	
+		for (int count =0; count <100000; count ++){
 		s.Player1();
+		if (w.Winner?() = true){
+		w.PrintWinner();
+		break;
+		break;
+		}
 		s.Player2();
+		if (w.Winner?() = true){
+			w.PrintWinner();
+			break;
+			break;
+			}
 
+	}
 	}
 
 }
